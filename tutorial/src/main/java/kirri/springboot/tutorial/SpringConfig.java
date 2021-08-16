c56a5b70a606381761d2aeda9a16854ac20c5000
cdcoc0 <jiyunee13@gmail.com>
2021-08-16 17:45:38 +0900

    spring data jpa

--- tutorial/src/main/java/kirri/springboot/tutorial/SpringConfig.java
+++ tutorial/src/main/java/kirri/springboot/tutorial/SpringConfig.java
@@ -1,5 +1,6 @@
 package kirri.springboot.tutorial;
 
+import kirri.springboot.tutorial.aop.TimeTraceAop;
 import kirri.springboot.tutorial.repository.*;
 import kirri.springboot.tutorial.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,19 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired //생성자 하나인 경우 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    //    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private DataSource dataSource;
 //
@@ -28,14 +35,19 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 }
