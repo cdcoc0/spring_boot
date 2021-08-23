b871148856536dc3a81ce3327c1b87b148fd765e
cdcoc0 <jiyunee13@gmail.com>
2021-08-23 17:40:48 +0900

    develop order entity business logic inside entity class

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,28 @@ public class OrderItem {
 
     private int orderPrice; //주문 시점에서의 상품 가격
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    //주문 취소
+    // == 재고 수량 복귀
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    //주문상품 전체 가격 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
