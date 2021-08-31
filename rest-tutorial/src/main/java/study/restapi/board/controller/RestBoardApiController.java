b6a32b71d41fcc1effe2219d065d4378b9ece845
cdcoc0 <jiyunee13@gmail.com>
2021-09-01 00:45:45 +0900

    simple rest api

---
+++ rest-tutorial/src/main/java/study/restapi/board/controller/RestBoardApiController.java
@@ -0,0 +1,47 @@
+package study.restapi.board.controller;
+
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.web.bind.annotation.*;
+import study.restapi.board.dto.BoardDto;
+import study.restapi.board.service.BoardService;
+
+import java.util.List;
+
+@RestController
+//Controller + ResponseBody
+public class RestBoardApiController {
+
+    private final BoardService boardService;
+
+    public RestBoardApiController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/api/board")
+    //@ResponseBody
+    public List<BoardDto> boardList() {
+        return boardService.selectBoardList();
+    }
+
+    @PostMapping("/api/board/write")
+    public void insertBoard(@RequestBody BoardDto board) {
+        boardService.insertBoard(board);
+    }
+
+    @GetMapping("/api/board/{boardIdx}")
+    public BoardDto readBoard(@PathVariable("boardIdx") int boardIdx) {
+        return boardService.selectBoard(boardIdx);
+    }
+
+    @PutMapping("/api/board/{boardIdx}")
+    public String updateBoard(@RequestBody BoardDto board) {
+        boardService.updateBoard(board);
+        return "redirect:/board";
+    }
+
+    @DeleteMapping("/api/board/{boardIdx}")
+    public String deleteboard(@PathVariable("boardIdx") int boardIdx) {
+        boardService.deleteBoard(boardIdx);
+        return "redirect:/board";
+    }
+}
