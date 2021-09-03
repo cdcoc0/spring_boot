617cb058369b1e3e7f0595b8a13464e30802f88b
cdcoc0 <jiyunee13@gmail.com>
2021-09-04 01:45:28 +0900

    REST create links with hateoas

--- spring-rest/src/main/java/spring/restapi/tutorial/controller/EmployeeController.java
+++ spring-rest/src/main/java/spring/restapi/tutorial/controller/EmployeeController.java
@@ -1,12 +1,18 @@
 package spring.restapi.tutorial.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 import spring.restapi.tutorial.domain.Employee;
 import spring.restapi.tutorial.exception.EmployeeNotFoundException;
 import spring.restapi.tutorial.repository.EmployeeRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +23,14 @@ public class EmployeeController {
     //Aggregate root
     //tag::get-aggregate-root[]
     @GetMapping("/employees")
-    public List<Employee> all() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<Employee>> all() { //
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     //end::get-aggregate-root[]
 
@@ -30,9 +42,12 @@ public class EmployeeController {
     //Single item
 
     @GetMapping("/employees/{id}")
-    public Employee one(@PathVariable Long id) {
-        return repository.findById(id)
+    public EntityModel<Employee> one(@PathVariable Long id) { //EntityModel<T> ==> 제네릭
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
@@ -52,5 +67,6 @@ public class EmployeeController {
     @DeleteMapping("employees/{id}")
     public void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
+        //update, delete 까지 에러 처리 해야함
     }
 }
