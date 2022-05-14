a11d4984680306c1f5cfe047e1a4c2ae2b2aaa7d
cdcoc0 <jiyunee13@gmail.com>
2022-05-14 20:48:07 +0900

    container & server port - replace tomcat with undertow - use server without web server using application.properties configuration - use random port

---
+++ webserver-exec/src/main/java/kirri/chop/webserver/PortListener.java
@@ -0,0 +1,17 @@
+package kirri.chop.webserver;
+
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PortListener implements ApplicationListener<ServletWebServerInitializedEvent> {
+    //웹서버가 초기화되면(생성되면) 이벤트 리스너가 호출됨
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+        ServletWebServerApplicationContext applicationContext = event.getApplicationContext();
+        System.out.println(applicationContext.getWebServer().getPort());
+    }
+}
